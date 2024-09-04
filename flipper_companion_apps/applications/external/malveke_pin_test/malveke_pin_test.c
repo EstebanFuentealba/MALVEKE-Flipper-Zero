b61b4f99cd1a627c99c313d601851b898d474303
Esteban Fuentealba <efuentealba@json.cl>
2024-09-04 08:50:12 -0400

    Update Test Application

--- flipper_companion_apps/applications/external/malveke_pin_test/malveke_pin_test.c
+++ flipper_companion_apps/applications/external/malveke_pin_test/malveke_pin_test.c
@@ -129,7 +129,7 @@ static UartEchoApp* gb_live_camera_app_alloc() {
 
     // View dispatcher
     app->view_dispatcher = view_dispatcher_alloc();
-    view_dispatcher_enable_queue(app->view_dispatcher);
+
     view_dispatcher_attach_to_gui(app->view_dispatcher, app->gui, ViewDispatcherTypeFullscreen);
 
     // Views
@@ -139,8 +139,7 @@ static UartEchoApp* gb_live_camera_app_alloc() {
     view_set_input_callback(app->view, gb_live_camera_view_input_callback);
     view_allocate_model(app->view, ViewModelTypeLocking, sizeof(UartDumpModel));
 
-    with_view_model(
-        app->view, UartDumpModel * model, { model->pin = 2; }, true);
+    with_view_model(app->view, UartDumpModel * model, { model->pin = 2; }, true);
 
     view_set_previous_callback(app->view, gb_live_camera_exit);
     view_dispatcher_add_view(app->view_dispatcher, 0, app->view);
