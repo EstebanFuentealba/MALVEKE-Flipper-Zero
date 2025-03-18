cf9a9d77aff627fda3eae486253dbb45fe83f650
fitoori <sbajaj@me.com>
2025-03-18 11:51:15 -0400

    Update gba_cartridge_app.c
    optimized

--- flipper_companion_apps/applications/external/malveke_gba_cartridge/gba_cartridge_app.c
+++ flipper_companion_apps/applications/external/malveke_gba_cartridge/gba_cartridge_app.c
@@ -12,7 +12,7 @@ void gba_cartridge_app_tick_event_callback(void* context) {
     scene_manager_handle_tick_event(app->scene_manager);
 }
 
-//leave app if back button pressed
+// Exit the app if the back button is pressed
 bool gba_cartridge_app_navigation_event_callback(void* context) {
     furi_assert(context);
     GBACartridge* app = context;
@@ -21,36 +21,38 @@ bool gba_cartridge_app_navigation_event_callback(void* context) {
 
 GBACartridge* gba_cartridge_app_app_alloc() {
     GBACartridge* app = malloc(sizeof(GBACartridge));
+    furi_assert(app != NULL); // Ensure allocation succeeded
     app->gui = furi_record_open(RECORD_GUI);
     app->storage = furi_record_open(RECORD_STORAGE);
     app->notification = furi_record_open(RECORD_NOTIFICATION);
 
-    //Turn backlight on, believe me this makes testing your app easier
+    // Turn on backlight to facilitate testing
     notification_message(app->notification, &sequence_display_backlight_on);
 
-    //Scene additions
+    // Set up the view dispatcher and scene manager
     app->view_dispatcher = view_dispatcher_alloc();
     view_dispatcher_enable_queue(app->view_dispatcher);
 
     app->scene_manager = scene_manager_alloc(&gba_cartridge_scene_handlers, app);
     view_dispatcher_set_event_callback_context(app->view_dispatcher, app);
-    view_dispatcher_set_navigation_event_callback(
-        app->view_dispatcher, gba_cartridge_app_navigation_event_callback);
-    view_dispatcher_set_tick_event_callback(
-        app->view_dispatcher, gba_cartridge_app_tick_event_callback, 100);
-    view_dispatcher_set_custom_event_callback(
-        app->view_dispatcher, gba_cartridge_app_custom_event_callback);
+    view_dispatcher_set_navigation_event_callback(app->view_dispatcher, gba_cartridge_app_navigation_event_callback);
+    view_dispatcher_set_tick_event_callback(app->view_dispatcher, gba_cartridge_app_tick_event_callback, 100);
+    view_dispatcher_set_custom_event_callback(app->view_dispatcher, gba_cartridge_app_custom_event_callback);
     app->submenu = variable_item_list_alloc();
 
     // Used for File Browser
     app->dialogs = furi_record_open(RECORD_DIALOGS);
     app->file_path = furi_string_alloc();
 
+    // Initialize UART interfaces
     app->uart = usart_init(app);
     app->lp_uart = lp_uart_init(app);
 
+    // Add views to the dispatcher
     view_dispatcher_add_view(
-        app->view_dispatcher, GBACartridgeViewIdMenu, variable_item_list_get_view(app->submenu));
+        app->view_dispatcher,
+        GBACartridgeViewIdMenu,
+        variable_item_list_get_view(app->submenu));
     app->gba_cartridge_startscreen = gba_cartridge_startscreen_alloc();
     view_dispatcher_add_view(
         app->view_dispatcher,
@@ -62,29 +64,31 @@ GBACartridge* gba_cartridge_app_app_alloc() {
         GBACartridgeViewIdScene1,
         gba_cartridge_scene_1_get_view(app->gba_cartridge_scene_1));
 
-    //  Enable 5v
-    // furi_hal_power_enable_otg();
-    // furi_delay_ms(1);
+    // Optionally enable 5V power if required (experimental)
+#ifdef ENABLE_5V
+    furi_hal_power_enable_otg();
+    furi_delay_ms(1);
+#endif
     return app;
 }
 
 void gba_cartridge_app_app_free(GBACartridge* app) {
     furi_assert(app);
-    // Views
+    // Remove views from the dispatcher
     view_dispatcher_remove_view(app->view_dispatcher, GBACartridgeViewIdMenu);
     view_dispatcher_remove_view(app->view_dispatcher, GBACartridgeViewIdScene1);
     view_dispatcher_remove_view(app->view_dispatcher, GBACartridgeViewIdStartscreen);
     variable_item_list_free(app->submenu);
 
-    // View Dispatcher
+    // Free the view dispatcher and scene manager
     view_dispatcher_free(app->view_dispatcher);
-    // Scene manager
     scene_manager_free(app->scene_manager);
 
-    //
+    // Free UART interfaces
     uart_free(app->uart);
     uart_free(app->lp_uart);
 
+    // Close system records
     furi_record_close(RECORD_GUI);
     furi_record_close(RECORD_STORAGE);
     furi_record_close(RECORD_DIALOGS);
@@ -95,13 +99,13 @@ void gba_cartridge_app_app_free(GBACartridge* app) {
 
     furi_string_free(app->file_path);
 
-    //Remove whatever is left
+    // Free the main application structure
     free(app);
 }
 
 int32_t gba_cartridge_app(void* p) {
     UNUSED(p);
-    // Disable expansion protocol to avoid interference with UART Handle
+    // Disable the expansion protocol to avoid interference with UART handling
     Expansion* expansion = furi_record_open(RECORD_EXPANSION);
     expansion_disable(expansion);
 
@@ -109,8 +113,8 @@ int32_t gba_cartridge_app(void* p) {
 
     view_dispatcher_attach_to_gui(app->view_dispatcher, app->gui, ViewDispatcherTypeFullscreen);
 
-    scene_manager_next_scene(
-        app->scene_manager, GBACartridgeSceneStartscreen); //Start with start screen
+    // Start with the start screen scene
+    scene_manager_next_scene(app->scene_manager, GBACartridgeSceneStartscreen);
 
     furi_hal_power_suppress_charge_enter();
 
@@ -119,7 +123,7 @@ int32_t gba_cartridge_app(void* p) {
     furi_hal_power_suppress_charge_exit();
     gba_cartridge_app_app_free(app);
 
-    // Return previous state of expansion
+    // Restore the expansion protocol state
     expansion_enable(expansion);
     furi_record_close(RECORD_EXPANSION);
 
